dcfb845167a055266525c50cc2810464aea658bf
juthomas <juthomas@student.42.fr>
2021-09-26 14:34:48 +0200

    [ADD] Gamepad fully functionnal

--- src/rcControl.js
+++ src/rcControl.js
@@ -1,25 +1,59 @@
 
-window.addEventListener("gamepadconnected", function(e) {
+window.addEventListener("gamepadconnected", function (e) {
 	console.log("Contrôleur n°%d connecté : %s. %d boutons, %d axes.",
-	e.gamepad.index, e.gamepad.id,
-	e.gamepad.buttons.length, e.gamepad.axes.length);
-  });
-  var controllers = {};
+		e.gamepad.index, e.gamepad.id,
+		e.gamepad.buttons.length, e.gamepad.axes.length);
+});
+var controllers = [];
 
-  function scangamepads() {
+function scangamepads() {
 	var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
-	for (var i = 0; i < gamepads.length; i++) {
-	  if (gamepads[i]) {
-
-		  controllers[gamepads[i].index] = gamepads[i];
-	  }
+	var i = 0
+	for (; i < gamepads.length; i++) {
+		if (gamepads[i]) {
+			
+			controllers[gamepads[i].index] = gamepads[i];
+		}
 	}
-  }
+	controllers[i] = {"id":"keyboard"}
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
+var currentControllerId = undefined;
+
+function selectInput(input){
+	if (input.id == "keyboard")
+	{
+		currentControllerId = undefined;
+	}
+	else 
+	{
+		currentControllerId = input.id;
+	}
+}
+
+const inputSelectBtn = document.getElementById('inputSelectBtn');
+inputSelectBtn.onclick = getInputSources;
+
+function getInputSources() {
+	scangamepads();
+
+	const inputOptionsMenu = Menu.buildFromTemplate(
+		controllers.map(input => {
+		  return {
+			label: input.id,
+			click: () => selectInput(input)
+		  };
+		})
+	  );
+		inputOptionsMenu.popup();
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
 	// Your code to run since DOM is loaded and ready
 	console.log("document loaded");
-	const {ipcRenderer} = require('electron');
+	const { ipcRenderer, remote } = require('electron');
+	const { dialog, Menu } = remote;
+
 
 	const upButton = document.getElementById('upButton');
 	const downButton = document.getElementById('downButton');
@@ -47,83 +81,61 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	let accelModifier = 0;
 	let turnModifier = 0;
 
-	// upButton.onclick = () => {
-	// 	// accelValue++;
-	// 	console.log(accelValue);
-	// }
-	// downButton.onclick = () => {
-	// 	// accelValue--;
-	// 	console.log(accelValue);
-
-	// }
-	// leftButton.onclick = () => {
-	// 	// turnValue--;		
-	// }
-	// rightButton.onclick = () => {
-	// 	// turnValue++;		
-	// }
-
-    upButton.addEventListener("mousedown", function(){
-        accelModifier = stepValue;
-    });
-
-    upButton.addEventListener("mouseup", function() {
-        accelModifier = 0;
-	});
-    downButton.addEventListener("mousedown", function(){
-        accelModifier = -stepValue;
-    });
-
-	downButton.addEventListener("mouseup", function() {
-        accelModifier = 0;
+	upButton.addEventListener("mousedown", function () {
+		accelModifier = stepValue;
 	});
 
-    leftButton.addEventListener("mousedown", function(){
-        turnModifier = stepValue;
-    });
-
-    leftButton.addEventListener("mouseup", function() {
-    	turnModifier = 0;
+	upButton.addEventListener("mouseup", function () {
+		accelModifier = 0;
 	});
-    rightButton.addEventListener("mousedown", function(){
-        turnModifier = -stepValue;
-    });
+	downButton.addEventListener("mousedown", function () {
+		accelModifier = -stepValue;
+	});
 
-	rightButton.addEventListener("mouseup", function() {
-        turnModifier = 0;
+	downButton.addEventListener("mouseup", function () {
+		accelModifier = 0;
+	});
+
+	leftButton.addEventListener("mousedown", function () {
+		turnModifier = stepValue;
+	});
+
+	leftButton.addEventListener("mouseup", function () {
+		turnModifier = 0;
+	});
+	rightButton.addEventListener("mousedown", function () {
+		turnModifier = -stepValue;
+	});
+
+	rightButton.addEventListener("mouseup", function () {
+		turnModifier = 0;
 	});
 
 
 	document.addEventListener("keyup", event => {
 		console.log("keyup : ", event.key);
-		if (event.key == "ArrowUp" || event.key == "ArrowDown")
-		{
+		if (event.key == "ArrowUp" || event.key == "ArrowDown") {
 			accelModifier = 0;
 		}
-		if (event.key == "ArrowLeft" || event.key == "ArrowRight")
-		{
+		if (event.key == "ArrowLeft" || event.key == "ArrowRight") {
 			turnModifier = 0;
 		}
 
 	});
-	  
+
 	document.addEventListener("keydown", event => {
 		console.log("keydown");
 
-		if (event.key == "ArrowUp")
-		{
+		if (event.key == "ArrowUp") {
 			accelModifier = stepValue;
 		}
-		else if (event.key == "ArrowDown")
-		{
+		else if (event.key == "ArrowDown") {
 			accelModifier = -stepValue;
 		}
-		else if (event.key == "ArrowLeft")
-		{
+		else if (event.key == "ArrowLeft") {
 			turnModifier = stepValue;
 		}
-		else if (event.key == "ArrowRight")
-		{
+		else if (event.key == "ArrowRight") {
 			turnModifier = -stepValue;
 		}
 	});
@@ -131,85 +143,75 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	window.setInterval(() => {
 
-		if (accelModifier)
-		{
-			if (accelModifier > 0)
-			{
+		if (accelModifier) {
+			if (accelModifier > 0) {
 				if (accelValue < 0)
 					accelValue = 0;
 				let addValue = accelModifier * (maxAccel * 1.5 - accelValue) / 100;
 				accelValue = accelValue + addValue > maxAccel ? maxAccel : accelValue + addValue;
 			}
-			else
-			{
+			else {
 				if (accelValue > 0)
 					accelValue = 0;
 				let addValue = accelModifier * (-minAccel * 1.5 + accelValue) / 100;
 				accelValue = accelValue + addValue < minAccel ? minAccel : accelValue + addValue;
 			}
 		}
-		else
-		{
-			if (accelValue >= 0)
-			{
+		else {
+			if (accelValue >= 0) {
 				accelValue = accelValue - stepValue < 0 ? 0 : accelValue - stepValue;
 			}
-			else
-			{
+			else {
 				accelValue = accelValue + stepValue > 0 ? 0 : accelValue + stepValue;
 			}
 		}
-		// console.log("turn modifier :", turnModifier);
-		if (turnModifier)
-		{
-			if (turnModifier > 0)
-			{
+		if (turnModifier) {
+			if (turnModifier > 0) {
 				if (turnValue < 0)
 					turnValue = 0;
 				let addValue = turnModifier * (maxTurn * 1.5 - turnValue) / 100;
 				turnValue = turnValue + addValue > maxTurn ? maxTurn : turnValue + addValue;
 			}
-			else
-			{
+			else {
 				if (turnValue > 0)
 					turnValue = 0;
 				let addValue = turnModifier * (-minTurn * 1.5 + turnValue) / 100;
 				turnValue = turnValue + addValue < minTurn ? minTurn : turnValue + addValue;
 			}
 		}
-		else
-		{
-			if (turnValue >= 0)
-			{
+		else {
+			if (turnValue >= 0) {
 				turnValue = turnValue - stepValue < 0 ? 0 : turnValue - stepValue;
 			}
-			else
-			{
+			else {
 				turnValue = turnValue + stepValue > 0 ? 0 : turnValue + stepValue;
 			}
+		}
+
+		scangamepads();
+		if (currentControllerId != undefined)
+		{
+			const currentController = controllers.find(elem => elem.id == currentControllerId);
+			accelValue = currentController.buttons[6].value > 0 ? currentController.buttons[6].value * -100 : currentController.buttons[7].value * 100
+			turnValue = currentController.axes[0] * -100;
+			accelValue = parseInt(accelValue, 10);
+			turnValue = parseInt(turnValue, 10);
 		}
 
 		upProgress.value = accelValue > 0 ? accelValue : 0;
 		downProgress.value = -accelValue > 0 ? -accelValue : 0;
 		leftProgress.value = turnValue > 0 ? turnValue : 0;
 		rightProgress.value = -turnValue > 0 ? -turnValue : 0;
-		// console.log("port", port.value, "ip", ipAddr.value);
-		// console.log("activate", udpActivated.checked);
-		if (udpActivated.checked == true)
-		{
+		if (udpActivated.checked == true) {
 
 			ipcRenderer.send('control:data', {
-				'accelValue':accelValue,
-				'turnValue':turnValue,
+				'accelValue': accelValue,
+				'turnValue': turnValue,
 				'port': parseInt(port.value, 10),
 				'ip': ipAddr.value
 			});
 		}
-		scangamepads();
-		// console.log(controllers);
-		gamepadAccel = controllers[0].buttons[7];
-		console.log(gamepadAccel);
-	}, 500);
+	}, 50);
 
 });
 
